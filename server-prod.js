d64f0b68b2d3ae0e96c008bef2e71ce7061b6aa1
Alexander Almström <alexander.almstrom@panagora.se>
2019-10-24 02:16:25 +0200

    Add helmet

--- server-prod.js
+++ server-prod.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'public'));
-app.set('view engine', 'pug');
+app.use(helmet());
 
 // parse application/json
 app.use(bodyParser.json());
@@ -19,5 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'pug');
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
